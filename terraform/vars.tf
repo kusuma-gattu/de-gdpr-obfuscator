fc138e948c13344050f125f55ec3c22ff981df89
kusuma-gattu <kusuma.gattu@outlook.com>
2024-12-21 20:51:24 +0000

    update sf name

---
+++ terraform/vars.tf
@@ -0,0 +1,17 @@
+# Define variables which used across the Terraform configurations
+
+variable "lambda_name" {
+    type = string
+    default = "obfuscator_tool"
+}
+
+variable "s3_bucket_name" {
+    type = string
+    # REPLACE HERE with YOUR S3 BUCKET NAME
+    default = "gdpr-ingestion-zone"
+}
+
+variable "step_function_name" {
+    type = string
+    default = "InvokeLambdaAndRetrieveFile"
+}
