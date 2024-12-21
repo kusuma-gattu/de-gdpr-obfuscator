799e11a46f558bf7fe9093c2da62642b0cb4c4e4
kusuma-gattu <kusuma.gattu@outlook.com>
2024-12-21 20:52:09 +0000

    use existing s3

---
+++ terraform/s3.tf
@@ -0,0 +1,38 @@
+# create S3 bucket
+# resource "aws_s3_bucket" "s3_ingestion_zone" {
+#     bucket_prefix = "${var.s3_bucket_name}-"
+
+#     tags = {
+#         Name = "GDPR-IngestionZone"
+#         Environment = "Dev"
+#     }
+# }
+
+# access existing s3 bucket
+data "aws_s3_bucket" "s3_ingestion_zone" {
+    bucket = "${var.s3_bucket_name}"
+} 
+
+
+# S3 policy document
+data "aws_iam_policy_document" "s3_policy_doc" {
+    statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBuckets",
+      "s3:PutObject",
+      "s3:ListObjectsV2",
+      "s3-object-lambda:*",
+    ]
+
+    resources =  [
+        "${data.aws_s3_bucket.s3_ingestion_zone.arn}/*"
+    ]
+  }
+}
+
+# define s3 policy
+resource "aws_iam_policy" "s3_policy" {
+    name = "s3_policy"
+    policy = data.aws_iam_policy_document.s3_policy_doc.json
+}
